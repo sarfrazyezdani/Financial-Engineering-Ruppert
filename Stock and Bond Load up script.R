12bdd0378c0737bcfaab6e4519a7e7e9cbb116ee
sarfrazyezdani <sarfrazyezdani@gmail.com>
2022-01-09 15:37:24 +0200

    Completing Problem 1 to 3 (GM Returns and Ford Returns, and Microsoft Returns and Merck Returns) of the Book's Chapter 2
    2.4 R-Lab - Problem 1 to 3

--- Stock and Bond Load up script.R
+++ Stock and Bond Load up script.R
@@ -14,4 +14,30 @@ n=dim(dat)[1]
 GMReturn= GM_AC[-1] / GM_AC[-n] -1
 FordReturn= F_AC[-1]/F_AC[-n] -1
 par(mfrow = c(1,1))
-plot(GMReturn,FordReturn, type="p")
+qplot(GMReturn,FordReturn, type="p")
+
+#Q.1 Do the GM and Ford returns seem positively correlated? Do you notice any
+#outlying returns? if "yes," do outlying G returns seem to occue with outlying
+#Ford Returns?
+
+cor(GMReturn,FordReturn)
+
+#Q.2 Compute the log returns for GM and plot the returns versus the log returns.
+#How highly correlated are the two types of returns?
+
+#Log returns
+
+GMLogR= log(GM_AC[-1]/GM_AC[-n])
+par(mfrow=c(1,1))
+qplot(GMReturn,GMLogR,type="l")
+cor(GMReturn,GMLogR)
+
+
+#Q.3 Repeat Problem 1 with Microsoft (MSFT) and Merck (MRK).
+
+MSFTReturn = MSFT_AC[-1]/MSFT_AC[-n] -1
+MRKReturn = MRK_AC[-1]/MRK_AC[-n] -1
+par(mfrow=c(1,1))
+qplot(MSFTReturn,MRKReturn, type="p")
+
+cor(MSFTReturn,MRKReturn)
